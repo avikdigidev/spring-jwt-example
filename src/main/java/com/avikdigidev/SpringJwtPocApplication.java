8c5218d9c6f858da5ba61d9956d0fd19f981f0cf
avikdigidev <avikdigidev@gmail.com>
2022-05-25 23:44:53 +0530

    Initial commit

---
+++ src/main/java/com/avikdigidev/SpringJwtPocApplication.java
@@ -0,0 +1,13 @@
+package com.avikdigidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avikdigidev/SpringJwtPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avikdigidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
